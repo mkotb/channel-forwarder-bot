186022e3d381b3cb5c955b5507524a01f109c6b4
Mazen Kotb <me@mazenk.com>
2020-08-29 21:04:46 -0700

    Determine web preview using message entities

--- src/main/kotlin/com/mazenk/channelforwarder/messages/handler/TextMessageHandler.kt
+++ src/main/kotlin/com/mazenk/channelforwarder/messages/handler/TextMessageHandler.kt
@@ -1,5 +1,6 @@
 package com.mazenk.channelforwarder.messages.handler
 
+import com.jtelegram.api.message.entity.MessageEntityType
 import com.jtelegram.api.message.impl.TextMessage
 import com.jtelegram.api.requests.message.edit.EditTextMessage
 import com.jtelegram.api.requests.message.framework.req.EditMessageRequest
@@ -9,10 +10,14 @@ import com.mazenk.channelforwarder.link.LinkData
 import com.mazenk.channelforwarder.messages.MessageHandler
 
 object TextMessageHandler: MessageHandler<TextMessage>(TextMessage::class) {
+    private fun shouldDisablePreview(message: TextMessage): Boolean {
+        return message.entities.none { it.type == MessageEntityType.TEXT_LINK }
+    }
+
     override fun forward(linkData: LinkData, message: TextMessage): SendableMessageRequest<TextMessage> {
         return SendText.builder()
                 .text(buildText(linkData, message.text, message))
-                .disableWebPagePreview(true)
+                .disableWebPagePreview(shouldDisablePreview(message))
                 .build()
     }
 
@@ -20,7 +25,7 @@ object TextMessageHandler: MessageHandler<TextMessage>(TextMessage::class) {
         return EditTextMessage.builder()
                 .messageId(forwardedMessageId)
                 .text(buildText(linkData, message.text, message))
-                .disableWebPagePreview(true)
+                .disableWebPagePreview(shouldDisablePreview(message))
                 .build()
     }
 
