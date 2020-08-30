6e446f2e931232975c496b018b531d284f9beace
Mazen Kotb <me@mazenk.com>
2020-08-29 17:10:04 -0700

    Add sample config and remove redundant attachment

--- src/main/kotlin/com/mazenk/channelforwarder/command/utils.kt
+++ src/main/kotlin/com/mazenk/channelforwarder/command/utils.kt
@@ -1,11 +1,10 @@
 package com.mazenk.channelforwarder.command
 
 import com.jtelegram.api.events.message.TextMessageEvent
-import com.jtelegram.api.kotlin.BotContext
 import com.jtelegram.api.kotlin.events.message.replyWith
 import com.mazenk.channelforwarder.GlobalContext
 
-suspend fun BotContext.isNotAuthorized(event: TextMessageEvent): Boolean {
+suspend fun isNotAuthorized(event: TextMessageEvent): Boolean {
     if (!GlobalContext.config.adminUserIds.contains(event.message.sender.id)) {
         event.replyWith("You are not authorized to use this bot!")
         return true
